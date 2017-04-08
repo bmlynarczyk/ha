cfd81724232e42128f2030518a4b0a630891cb33
Bartłomiej Młynarczyk <bmlynarczyk.introsoft@gmail.com>
2017-04-08 22:22:32 +0200

    add zuul as gateway

--- service/src/main/java/com/introsoft/service/ServiceApplication.java
+++ service/src/main/java/com/introsoft/service/ServiceApplication.java
@@ -2,10 +2,11 @@ package com.introsoft.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class ServiceApplication {
 
     public static void main(String[] args) {
