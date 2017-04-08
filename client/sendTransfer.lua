d5f1751d2d43d23770ab9e71a8c5fc881e5a4429
Bartłomiej Młynarczyk <bmlynarczyk.introsoft@gmail.com>
2017-04-08 18:45:41 +0200

    refactor containers for ldi purpose

--- client/sendTransfer.lua
+++ client/sendTransfer.lua
@@ -1,3 +1,3 @@
 wrk.method = "POST"
-wrk.body   = "{}"
+wrk.body   = "{\"amount\":\"100\", \"title\":\"ldi2017\", \"accountingDate\":1491664090, \"payeeAccount\":\"PL19116095884689260483003902\", \"beneficiaryName\":\"Bartlomiej Młynarczyk\", \"beneficiaryAccount\":\"PL80114059918954093015605127\"}"
 wrk.headers["Content-Type"] = "application/json"
